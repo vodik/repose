212ef9c8e8bb94fb7c2809234110fd1c6284c702
Simon Gomizelj <simongmzlj@gmail.com>
2013-06-05 19:25:00 -0400

    cleanup base64_decode

--- alpm/base64.c
+++ alpm/base64.c
@@ -129,63 +129,56 @@ int base64_encode(unsigned char **dst, const unsigned char *src, size_t len)
 /*
  * Decode a base64-formatted buffer
  */
-int base64_decode( unsigned char *dst, size_t *dlen,
-                   const unsigned char *src, size_t len )
+int base64_decode(unsigned char **dst, const unsigned char *src, size_t len)
 {
     size_t i, n;
     uint32_t j, x;
     unsigned char *p;
+    unsigned char *mem;
 
-    for( i = j = n = 0; i < len; i++ )
-    {
-        if( ( len - i ) >= 2 &&
-            src[i] == '\r' && src[i + 1] == '\n' )
+    for(i = j = n = 0; i < len; i++) {
+        if(len - i >= 2 && src[i] == '\r' && src[i + 1] == '\n')
             continue;
 
-        if( src[i] == '\n' )
+        if(src[i] == '\n')
             continue;
 
-        if( src[i] == '=' && ++j > 2 )
-            return( POLARSSL_ERR_BASE64_INVALID_CHARACTER );
+        if(src[i] == '=' && ++j > 2)
+            return POLARSSL_ERR_BASE64_INVALID_CHARACTER;
 
-        if( src[i] > 127 || base64_dec_map[src[i]] == 127 )
-            return( POLARSSL_ERR_BASE64_INVALID_CHARACTER );
+        if(src[i] > 127 || base64_dec_map[src[i]] == 127)
+            return POLARSSL_ERR_BASE64_INVALID_CHARACTER;
 
-        if( base64_dec_map[src[i]] < 64 && j != 0 )
-            return( POLARSSL_ERR_BASE64_INVALID_CHARACTER );
+        if(base64_dec_map[src[i]] < 64 && j != 0)
+            return POLARSSL_ERR_BASE64_INVALID_CHARACTER;
 
         n++;
     }
 
-    if( n == 0 )
-        return( 0 );
+    if(n == 0 )
+        return 0;
 
     n = ((n * 6) + 7) >> 3;
+    mem = malloc(n);
 
-    if( *dlen < n )
-    {
-        *dlen = n;
-        return( POLARSSL_ERR_BASE64_BUFFER_TOO_SMALL );
-    }
-
-   for( j = 3, n = x = 0, p = dst; i > 0; i--, src++ )
-   {
-        if( *src == '\r' || *src == '\n' )
+   for(j = 3, n = x = 0, p = mem; i > 0; i--, src++) {
+        if(*src == '\r' || *src == '\n')
             continue;
 
-        j -= ( base64_dec_map[*src] == 64 );
-        x  = (x << 6) | ( base64_dec_map[*src] & 0x3F );
+        j -= (base64_dec_map[*src] == 64);
+        x  = (x << 6) | (base64_dec_map[*src] & 0x3F);
 
-        if( ++n == 4 )
-        {
+        if(++n == 4) {
             n = 0;
-            if( j > 0 ) *p++ = (unsigned char)( x >> 16 );
-            if( j > 1 ) *p++ = (unsigned char)( x >>  8 );
-            if( j > 2 ) *p++ = (unsigned char)( x       );
+            if(j > 0)
+                *p++ = (unsigned char)(x >> 16);
+            if(j > 1)
+                *p++ = (unsigned char)(x >> 8);
+            if(j > 2)
+                *p++ = (unsigned char)x;
         }
     }
 
-    *dlen = p - dst;
-
-    return( 0 );
+   *dst = mem;
+    return 0;
 }
--- alpm/base64.h
+++ alpm/base64.h
@@ -25,28 +25,9 @@
 #ifndef _BASE64_H
 #define _BASE64_H
 
-#define POLARSSL_ERR_BASE64_BUFFER_TOO_SMALL               -0x0010  /**< Output buffer too small. */
-#define POLARSSL_ERR_BASE64_INVALID_CHARACTER              -0x0012  /**< Invalid character in input. */
+#define POLARSSL_ERR_BASE64_INVALID_CHARACTER -0x0012  /**< Invalid character in input. */
 
 int base64_encode(unsigned char **dst, const unsigned char *src, size_t len);
-
-/**
- * \brief          Decode a base64-formatted buffer
- *
- * \param dst      destination buffer
- * \param dlen     size of the buffer
- * \param src      source buffer
- * \param slen     amount of data to be decoded
- *
- * \return         0 if successful, POLARSSL_ERR_BASE64_BUFFER_TOO_SMALL, or
- *                 POLARSSL_ERR_BASE64_INVALID_DATA if the input data is not
- *                 correct. *dlen is always updated to reflect the amount
- *                 of data that has (or would have) been written.
- *
- * \note           Call this function with *dlen = 0 to obtain the
- *                 required buffer size in *dlen
- */
-int base64_decode( unsigned char *dst, size_t *dlen,
-                   const unsigned char *src, size_t slen );
+int base64_decode(unsigned char **dst, const unsigned char *src, size_t len);
 
 #endif /* base64.h */
