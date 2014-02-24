55eba4353a787743149acb891f3387fecd9b0e3a
Simon Gomizelj <simongmzlj@gmail.com>
2014-02-24 15:45:50 -0500

    add licences to code

--- desc.h
+++ desc.h
@@ -1,13 +1,25 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) Simon Gomizelj, 2014
+ */
+
 #pragma once
 
 #include "desc.h"
 #include "package.h"
 #include <archive.h>
 #include <archive_entry.h>
-
-/* struct desc_parser { */
-/*     int cs, act; */
-/*     char *ts, *te; */
-/* }; */
 
 void read_desc(struct archive *archive, struct pkg *pkg);
