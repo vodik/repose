1b4acbc9b2e55fbec5fba191c0682b6e18bbf9c2
Simon Gomizelj <simongmzlj@gmail.com>
2015-03-02 22:30:45 -0500

    Make use of the sentinel attribute

--- src/util.h
+++ src/util.h
@@ -28,6 +28,7 @@
 #define _noreturn_          __attribute__((noreturn))
 #define _cleanup_(x)        __attribute__((cleanup(x)))
 #define _printf_(a,b)       __attribute__((format (printf, a, b)))
+#define _sentinel_          __attribute__((sentinel))
 #define _cleanup_free_      _cleanup_(freep)
 #define _cleanup_fclose_    _cleanup_(fclosep)
 #define _cleanup_closedir_  _cleanup_(closedirp)
@@ -41,7 +42,7 @@ static inline void closep(int *fd)     { if (*fd >= 0) close(*fd); }
 static inline void *zero(void *s, size_t n) { return memset(s, 0, n); }
 static inline bool streq(const char *s1, const char *s2) { return strcmp(s1, s2) == 0; }
 
-char *joinstring(const char *root, ...);
+char *joinstring(const char *root, ...) _sentinel_;
 
 int xstrtol(const char *str, long *out);
 int xstrtoul(const char *str, unsigned long *out);
