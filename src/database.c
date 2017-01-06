6d4ea777bf4e0f0bbc089806316812effbbc9e58
Simon Gomizelj <simon@vodik.xyz>
2017-01-05 23:58:09 -0500

    Refactor database handling code organization
    More consistent and informative naming and style to help keep the module
    maintainable.

--- src/database.c
+++ src/database.c
@@ -23,15 +23,22 @@
 #include "buffer.h"
 #include "signing.h"
 
-struct db {
-    int fd;
+struct database_reader {
     struct archive *archive;
-    time_t mtime;
     struct pkg *likely_pkg;
+    time_t mtime;
+};
+
+struct database_writer {
+    struct archive *archive;
+    struct archive_entry *entry;
+    struct buffer buf;
+    enum contents contents;
+    int poolfd;
 };
 
 /* The name, version and type fields all share the same memory */
-struct dbentry {
+struct entry_info {
     char *name;
     const char *type;
     const char *version;
@@ -63,29 +70,7 @@ static char *sha256_file(int dirfd, const char *filename)
     return sha256_fd(fd);
 }
 
-static int open_database(struct db *db, int fd)
-{
-    struct stat st;
-    check_posix(fstat(fd, &st), "failed to stat database");
-
-    *db = (struct db){
-        .archive = archive_read_new(),
-        .fd = fd,
-        .mtime = st.st_mtime
-    };
-
-    archive_read_support_filter_all(db->archive);
-    archive_read_support_format_all(db->archive);
-
-    if (archive_read_open_fd(db->archive, fd, 8192) != ARCHIVE_OK) {
-        archive_read_close(db->archive);
-        archive_read_free(db->archive);
-    }
-
-    return 0;
-}
-
-static int parse_database_pathname(const char *entryname, struct dbentry *entry)
+static int parse_database_pathname(const char *entryname, struct entry_info *entry)
 {
     entry->name = strdup(entryname);
     const char *slash = strchrnul(entry->name, '/'), *dash = slash;
@@ -103,32 +88,32 @@ static int parse_database_pathname(const char *entryname, struct dbentry *entry)
     return 0;
 }
 
-static inline void dbentry_free(struct dbentry *dbentry)
+static inline void entry_info_free(struct entry_info *entry_info)
 {
-    free(dbentry->name);
+    free(entry_info->name);
 }
 
-static struct pkg *get_package(struct db *db, struct dbentry *dbentry,
+static struct pkg *get_package(struct database_reader *db, struct entry_info *entry_info,
                                struct pkgcache **pkgcache, bool allocate)
 {
     struct pkg *pkg;
 
     if (db->likely_pkg) {
-        hash_t pkgname_hash = sdbm(dbentry->name);
-        if (pkgname_hash == db->likely_pkg->hash && streq(db->likely_pkg->name, dbentry->name))
+        hash_t pkgname_hash = sdbm(entry_info->name);
+        if (pkgname_hash == db->likely_pkg->hash && streq(db->likely_pkg->name, entry_info->name))
             return db->likely_pkg;
     }
 
-    pkg = pkgcache_find(*pkgcache, dbentry->name);
+    pkg = pkgcache_find(*pkgcache, entry_info->name);
     if (allocate && !pkg) {
         pkg = malloc(sizeof(struct pkg));
         if (!pkg)
             return NULL;
 
         *pkg = (struct pkg){
-            .hash = sdbm(dbentry->name),
-            .name = strdup(dbentry->name),
-            .version = strdup(dbentry->version),
+            .hash = sdbm(entry_info->name),
+            .name = strdup(entry_info->name),
+            .version = strdup(entry_info->version),
             .mtime = db->mtime
         };
 
@@ -141,7 +126,7 @@ static struct pkg *get_package(struct db *db, struct dbentry *dbentry,
     return pkg;
 }
 
-static bool is_database_metadata(const char *entry_name)
+static bool is_database_metadata(const char *filename)
 {
     static const char *metadata_names[] = {
         "desc",
@@ -151,29 +136,29 @@ static bool is_database_metadata(const char *entry_name)
     };
 
     for (const char **n = metadata_names; *n; ++n) {
-        if (streq(entry_name, *n))
+        if (streq(filename, *n))
             return true;
     }
 
     return false;
 }
 
-static int parse_database_entry(struct db *db, struct archive_entry *entry,
+static int parse_database_entry(struct database_reader *db, struct archive_entry *entry,
                                 struct pkgcache **pkgcache)
 {
     const char *pathname = archive_entry_pathname(entry);
-    struct dbentry dbentry;
+    struct entry_info entry_info;
     int ret = 0;
 
-    if (parse_database_pathname(pathname, &dbentry) < 0) {
+    if (parse_database_pathname(pathname, &entry_info) < 0) {
         ret = -1;
         goto cleanup;
     }
 
-    if (dbentry.type && is_database_metadata(dbentry.type)) {
-        bool allocate = !streq(dbentry.type, "files");
+    if (entry_info.type && is_database_metadata(entry_info.type)) {
+        bool allocate = !streq(entry_info.type, "files");
 
-        struct pkg *pkg = get_package(db, &dbentry, pkgcache, allocate);
+        struct pkg *pkg = get_package(db, &entry_info, pkgcache, allocate);
         if (allocate && !pkg) {
             ret = -1;
             goto cleanup;
@@ -185,29 +170,72 @@ static int parse_database_entry(struct db *db, struct archive_entry *entry,
     }
 
 cleanup:
-    dbentry_free(&dbentry);
+    entry_info_free(&entry_info);
     return ret;
 }
 
 int load_database(int fd, struct pkgcache **pkgcache)
 {
-    struct db db;
-    struct archive_entry *entry;
+    int ret = 0;
 
-    if (open_database(&db, fd) < 0)
-        return -1;
+    struct stat st;
+    check_posix(fstat(fd, &st), "failed to stat database");
+
+    struct archive_entry *entry;
+    struct database_reader db = {
+        .archive = archive_read_new(),
+        .mtime = st.st_mtime
+    };
+
+    archive_read_support_filter_all(db.archive);
+    archive_read_support_format_all(db.archive);
+
+    if (archive_read_open_fd(db.archive, fd, 8192) != ARCHIVE_OK) {
+        ret = -1;
+        goto cleanup;
+    }
 
     while (archive_read_next_header(db.archive, &entry) == ARCHIVE_OK) {
         const mode_t mode = archive_entry_mode(entry);
 
         if (S_ISREG(mode) && parse_database_entry(&db, entry, pkgcache) < 0) {
-            archive_read_close(db.archive);
-            archive_read_free(db.archive);
-            return -1;
+            ret = -1;
+            goto cleanup;
         }
     }
 
-    return 0;
+cleanup:
+    archive_read_close(db.archive);
+    archive_read_free(db.archive);
+    return ret;
+}
+
+static void archive_entry_populate(struct archive_entry *e, unsigned int type,
+                                   const char *path, mode_t mode)
+{
+    time_t now = time(NULL);
+
+    archive_entry_set_pathname(e, path);
+    archive_entry_set_filetype(e, type);
+    archive_entry_set_perm(e, mode);
+    archive_entry_set_uname(e, "repose");
+    archive_entry_set_gname(e, "repose");
+    archive_entry_set_ctime(e, now, 0);
+    archive_entry_set_mtime(e, now, 0);
+    archive_entry_set_atime(e, now, 0);
+}
+
+static void commit_entry(struct database_writer *db, const char *name,
+                         const char *folder)
+{
+    _cleanup_free_ char *entrypath = joinstring(folder, "/", name, NULL);
+
+    archive_entry_populate(db->entry, AE_IFREG, entrypath, 0644);
+    archive_entry_set_size(db->entry, db->buf.len);
+    archive_write_header(db->archive, db->entry);
+    archive_write_data(db->archive, db->buf.data, db->buf.len);
+    archive_entry_clear(db->entry);
+    buffer_clear(&db->buf);
 }
 
 static void write_list(struct buffer *buf, const char *header, const alpm_list_t *lst)
@@ -245,150 +273,123 @@ static void write_time(struct buffer *buf, const char *header, time_t val)
     size_t: write_size, \
     time_t: write_time)(buf, header, val)
 
-static void compile_depends_entry(struct pkg *pkg, struct buffer *buf)
+static void compile_desc_entry(struct database_writer *db, struct pkg *pkg)
 {
-    write_entry(buf, "DEPENDS",      pkg->depends);
-    write_entry(buf, "CONFLICTS",    pkg->conflicts);
-    write_entry(buf, "PROVIDES",     pkg->provides);
-    write_entry(buf, "OPTDEPENDS",   pkg->optdepends);
-    write_entry(buf, "MAKEDEPENDS",  pkg->makedepends);
-    write_entry(buf, "CHECKDEPENDS", pkg->checkdepends);
-}
-
-static void compile_desc_entry(struct pkg *pkg, struct buffer *buf, int poolfd)
-{
-    write_entry(buf, "FILENAME",  pkg->filename);
-    write_entry(buf, "NAME",      pkg->name);
-    write_entry(buf, "BASE",      pkg->base);
-    write_entry(buf, "VERSION",   pkg->version);
-    write_entry(buf, "DESC",      pkg->desc);
-    write_entry(buf, "GROUPS",    pkg->groups);
-    write_entry(buf, "CSIZE",     pkg->size);
-    write_entry(buf, "ISIZE",     pkg->isize);
+    write_entry(&db->buf, "FILENAME",  pkg->filename);
+    write_entry(&db->buf, "NAME",      pkg->name);
+    write_entry(&db->buf, "BASE",      pkg->base);
+    write_entry(&db->buf, "VERSION",   pkg->version);
+    write_entry(&db->buf, "DESC",      pkg->desc);
+    write_entry(&db->buf, "GROUPS",    pkg->groups);
+    write_entry(&db->buf, "CSIZE",     pkg->size);
+    write_entry(&db->buf, "ISIZE",     pkg->isize);
 
     if (pkg->base64sig) {
-        write_entry(buf, "PGPSIG", pkg->base64sig);
+        write_entry(&db->buf, "PGPSIG", pkg->base64sig);
     } else {
         if (!pkg->sha256sum)
-            pkg->sha256sum = sha256_file(poolfd, pkg->filename);
-        write_entry(buf, "SHA256SUM", pkg->sha256sum);
+            pkg->sha256sum = sha256_file(db->poolfd, pkg->filename);
+        write_entry(&db->buf, "SHA256SUM", pkg->sha256sum);
     }
 
-    write_entry(buf, "URL",       pkg->url);
-    write_entry(buf, "LICENSE",   pkg->licenses);
-    write_entry(buf, "ARCH",      pkg->arch);
-    write_entry(buf, "BUILDDATE", pkg->builddate);
-    write_entry(buf, "PACKAGER",  pkg->packager);
-    write_entry(buf, "REPLACES",  pkg->replaces);
+    write_entry(&db->buf, "URL",       pkg->url);
+    write_entry(&db->buf, "LICENSE",   pkg->licenses);
+    write_entry(&db->buf, "ARCH",      pkg->arch);
+    write_entry(&db->buf, "BUILDDATE", pkg->builddate);
+    write_entry(&db->buf, "PACKAGER",  pkg->packager);
+    write_entry(&db->buf, "REPLACES",  pkg->replaces);
 }
 
-static void compile_files_entry(struct pkg *pkg, struct buffer *buf, int poolfd)
+static void compile_depends_entry(struct database_writer *db, struct pkg *pkg)
+{
+    write_entry(&db->buf, "DEPENDS",      pkg->depends);
+    write_entry(&db->buf, "CONFLICTS",    pkg->conflicts);
+    write_entry(&db->buf, "PROVIDES",     pkg->provides);
+    write_entry(&db->buf, "OPTDEPENDS",   pkg->optdepends);
+    write_entry(&db->buf, "MAKEDEPENDS",  pkg->makedepends);
+    write_entry(&db->buf, "CHECKDEPENDS", pkg->checkdepends);
+}
+
+static void compile_files_entry(struct database_writer *db, struct pkg *pkg)
 {
     if (!pkg->files) {
-        _cleanup_close_ int pkgfd = openat(poolfd, pkg->filename, O_RDONLY);
+        _cleanup_close_ int pkgfd = openat(db->poolfd, pkg->filename, O_RDONLY);
         if (pkgfd < 0 && errno != ENOENT)
             err(EXIT_FAILURE, "failed to open %s", pkg->filename);
 
         load_package_files(pkg, pkgfd);
     }
 
-    write_entry(buf, "FILES", pkg->files);
+    write_entry(&db->buf, "FILES", pkg->files);
 }
 
-static void archive_entry_populate(struct archive_entry *e, unsigned int type,
-                                   const char *path, mode_t mode)
+static void compile_database_entry(struct database_writer *db, struct pkg *pkg)
 {
-    time_t now = time(NULL);
+    _cleanup_free_ char *folder = joinstring(pkg->name, "-", pkg->version, NULL);
 
-    archive_entry_set_pathname(e, path);
-    archive_entry_set_filetype(e, type);
-    archive_entry_set_perm(e, mode);
-    archive_entry_set_uname(e, "repose");
-    archive_entry_set_gname(e, "repose");
-    archive_entry_set_ctime(e, now, 0);
-    archive_entry_set_mtime(e, now, 0);
-    archive_entry_set_atime(e, now, 0);
-}
+    archive_entry_populate(db->entry, AE_IFDIR, folder, 0755);
+    archive_write_header(db->archive, db->entry);
+    archive_entry_clear(db->entry);
 
-static void record_entry(struct archive *archive, struct archive_entry *e,
-                         const char *root, const char *entry, struct buffer *buf)
-{
-    _cleanup_free_ char *entrypath = joinstring(root, "/", entry, NULL);
-
-    archive_entry_populate(e, AE_IFREG, entrypath, 0644);
-    archive_entry_set_size(e, buf->len);
-    archive_write_header(archive, e);
-    archive_write_data(archive, buf->data, buf->len);
-    archive_entry_clear(e);
-    buffer_clear(buf);
-}
-
-static void compile_database_entry(struct archive *archive, struct archive_entry *e, struct pkg *pkg,
-                                   int contents, struct buffer *buf, int poolfd)
-{
-    _cleanup_free_ char *entrypath = joinstring(pkg->name, "-", pkg->version, NULL);
-
-    archive_entry_populate(e, AE_IFDIR, entrypath, 0755);
-    archive_write_header(archive, e);
-    archive_entry_clear(e);
-
-    if (contents & DB_DESC) {
-        compile_desc_entry(pkg, buf, poolfd);
-        record_entry(archive, e, entrypath, "desc", buf);
+    if (db->contents & DB_DESC) {
+        compile_desc_entry(db, pkg);
+        commit_entry(db, "desc", folder);
     }
-    if (contents & DB_DEPENDS) {
-        compile_depends_entry(pkg, buf);
-        record_entry(archive, e, entrypath, "depends", buf);
+    if (db->contents & DB_DEPENDS) {
+        compile_depends_entry(db, pkg);
+        commit_entry(db, "depends", folder);
     }
-    if (contents & DB_FILES) {
-        compile_files_entry(pkg, buf, poolfd);
-        record_entry(archive, e, entrypath, "files", buf);
+    if (db->contents & DB_FILES) {
+        compile_files_entry(db, pkg);
+        commit_entry(db, "files", folder);
     }
 }
 
 static int compile_database(struct repo *repo, const char *repo_name,
                             enum contents what)
 {
+    int ret = 0;
     _cleanup_close_ int dbfd = openat(repo->rootfd, repo_name,
                                       O_CREAT | O_WRONLY | O_TRUNC, 0644);
     if (dbfd < 0)
         return -1;
 
-    int ret = 0;
-    struct archive *archive = archive_write_new();
-    struct archive_entry *entry = archive_entry_new();
+    struct database_writer db = {
+        .archive = archive_write_new(),
+        .entry = archive_entry_new(),
+        .buf = {0},
+        .contents = what,
+        .poolfd = repo->poolfd,
+    };
 
-    archive_write_add_filter(archive, config.compression);
-    archive_write_set_format_pax_restricted(archive);
+    archive_write_add_filter(db.archive, config.compression);
+    archive_write_set_format_pax_restricted(db.archive);
 
-    if (archive_write_open_fd(archive, dbfd) < 0) {
+    if (archive_write_open_fd(db.archive, dbfd) < 0) {
         ret = -1;
         goto cleanup;
     }
 
-    archive_entry_populate(entry, AE_IFDIR, "", 0755);
-    archive_write_header(archive, entry);
-    archive_entry_clear(entry);
+    archive_entry_populate(db.entry, AE_IFDIR, "", 0755);
+    archive_write_header(db.archive, db.entry);
+    archive_entry_clear(db.entry);
 
-    struct buffer buf = {0};
+    /* The files database can get very, very large. Lets allocate a
+     * 2MiB buffer so we have plenty of room and avoid reallocation. */
+    buffer_reserve(&db.buf, 0x200000);
 
-    /* The files database can get very, very large. Lets preallocate
-     * a 2MiB buffer so we have plenty of room and avoid lots of
-     * rallocations. */
-    buffer_reserve(&buf, 0x200000);
-
-    alpm_list_t *pkg, *pkgs = repo->cache->list;
-    for (pkg = pkgs; pkg; pkg = pkg->next) {
-        struct pkg *metadata = pkg->data;
-        compile_database_entry(archive, entry, metadata, what, &buf, repo->poolfd);
+    const alpm_list_t *node;
+    for (node = repo->cache->list; node; node = node->next) {
+        struct pkg *pkg = node->data;
+        compile_database_entry(&db, pkg);
     }
 
-    archive_write_close(archive);
-    buffer_release(&buf);
+    archive_write_close(db.archive);
+    buffer_release(&db.buf);
 
 cleanup:
-    archive_entry_free(entry);
-    archive_write_free(archive);
+    archive_entry_free(db.entry);
+    archive_write_free(db.archive);
     return ret;
 }
 
